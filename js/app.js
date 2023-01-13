0c861417233829b2870044388783e6a049b6b9c5
AhmedElmeghalawy <52879089+AhmedElmeghalawy@users.noreply.github.com>
2023-01-13 17:39:02 +0200

    Added highlight to nav bar buttons

--- js/app.js
+++ js/app.js
@@ -1,66 +1,92 @@
-
-
-
-
 //getting the navBar element, so we would later append to it new created nav buttons acording to how many section on the page.
 let navBar = document.querySelector("#navbar__list");
 
 //getting the current sections on the html page, And based on how many of them, we would create the nav buttons.
 let sections = document.getElementsByTagName("section");
 
-
-
-
-//now we iterate through each section to create the correspond nav button for it
-for (let index = 0; index < sections.length; index++) {
-
-  // creating a new button using a function and passing the argument that passes the name and the function that we want to call when clickin gon that button.
-  // the {behavior:"smooth"} part is adding a smooth transition animation when scrolling.
- let newButton = createNavButton(sections[index].getAttribute("data-nav")
-                              , function () {sections[index].scrollIntoView({ behavior: "smooth" })});
-
-  //appending  the above element(newButton) to the navbar as its child
-  navBar.appendChild(newButton);
-}
-
-
-let addNewSectionButton = document.querySelector("#add_new_section");
-
+// let addNewSectionButton = document.querySelector("#add_new_section");
 let sectionsParent = document.querySelector("#sectionsParent");
 
+//first we populate the nave bar based on how many sections there are in the html page..
+PopulateNavBar();
 
-addNewSectionButton.onclick = function() {
-  let newSection =  createSection("New Section");
-  sectionsParent.appendChild(newSection);
+//secoundly we add A new Button To the list of the navbar buttons for creating new sections
+createAddNewSectionButton();
+
+//now when we interract with the add new section button..
+let newSectionCount = 0;
+function createAddNewSectionButton() {
+  //creating the "adding new section button"
+  let addNewSectionButton = createNavButton("[+]");
+
+  //adding the "createSection" method to the button when we click it
+  addNewSectionButton.onclick = function () {
+    newSectionCount++;
+
+    //creating new section and adding a number to it for the navigationButton to use later
+    let newSection = createSection("Section " + newSectionCount);
+    //appending the new section to "sectionsParent"
+    sectionsParent.appendChild(newSection);
+
+    //after we added the new section, we call the "PopulateNavBar" again, so it would read all the sections including the one we created above,
+    //we do that so it would create a new navbar button that corspond to the new section
+    PopulateNavBar();
+
+    //the addNewSectionButton([+]) get destroyed every time we calle the PopulateNavBar()...
+    //so we create it again ...
+    createAddNewSectionButton();
+
+    //Notice that:
+    //The above code excuted only on click..
+    //So basicly the button destroy itself then recreate itself again on click..
+  };
+
+  //after creating the section append its nav button to the navBar.
+  navBar.appendChild(addNewSectionButton);
 }
 
+function PopulateNavBar() {
+  //remove everything that in the navBar;
+  navBar.innerHTML = "";
 
-function createSection(SectionName){
+  //now we iterate through each section to create the correspond nav button for it
+  for (let index = 0; index < sections.length; index++) {
+    // creating a new button using a function and passing the argument that passes the name and the function that we want to call when clickin gon that button.
+    // the {behavior:"smooth"} part is adding a smooth transition animation when scrolling.
+    let newButton = createNavButton(
+      sections[index].getAttribute("data-nav"),
+      function () {
+        sections[index].scrollIntoView({ behavior: "smooth" });
+      }
+    );
 
-  let sectionTemplate = `<section id="${SectionName}" data-nav="${SectionName}">
-        <div class="landing__container">
-          <h2>${SectionName}</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
+    //appending  the above element(newButton) to the navbar as its child
+    navBar.appendChild(newButton);
+  }
+}
 
-          <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
-        </div>
-      </section>`;
+function createSection(SectionName) {
+  let container = document.createElement("section");
+  container.id = SectionName.split(" ").join("");
+  container.dataset.nav = SectionName.split(" ").join("");
 
-      let container = document.createElement("div");
-      container.innerHTML = sectionTemplate;
+  container.innerHTML = `<div class="landing__container">
+        <h2>${SectionName}</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
+
+        <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+      </div>`;
 
   return container;
 }
 
-
-
-function createNavButton(Name,ExcuteOnClick){
-
+function createNavButton(Name, ExcuteOnClick) {
   let sectionNavigationButton = null;
   //create button element
   sectionNavigationButton = document.createElement("li");
   //adding the class name "menu__link" in the css file to style the element
   sectionNavigationButton.classList.add("menu__link");
+  sectionNavigationButton.setAttribute("data-nav", Name);
   //setting the button name as the section title by extracting the name from its attribute "data-nav"
   sectionNavigationButton.innerText = Name;
   //changing the css style of the button to make it horizontal instead of vertical
@@ -74,38 +100,45 @@ function createNavButton(Name,ExcuteOnClick){
   return sectionNavigationButton;
 }
 
-
-
-//listing when scrolling happend so we would call 
-document.addEventListener("scroll", function() { MakeCurrentViewdElementOnScreenActive();});
+//listing when scrolling happend so we would call
+document.addEventListener("scroll", function () {
+  MakeCurrentViewdElementOnScreenActive();
+});
 
 //addin ActiveClass to the current viewd element(Section Element)
-function MakeCurrentViewdElementOnScreenActive(){
-    let topValue = window.innerHeight;
-    let bottomValue = 150;
-    for (let index = 0; index < sections.length; index++) {
-        let sectionElementAreaBox = sections[index].getBoundingClientRect();
+function MakeCurrentViewdElementOnScreenActive() {
+  let topValue = 150;
+  let bottomValue = 150;
+  for (let index = 0; index < sections.length; index++) {
+    let sectionElementAreaBox = sections[index].getBoundingClientRect();
 
-        if (sectionElementAreaBox.top <= topValue && sectionElementAreaBox.bottom >= bottomValue) {
-            sections[index].classList.add("your-active-class");
-            } else {
-            sections[index].classList.remove("your-active-class");
-            }
-
+    if (
+      sectionElementAreaBox.top <= topValue &&
+      sectionElementAreaBox.bottom >= bottomValue
+    ) {
+      sections[index].classList.add("your-active-class");
+      activeLink = navBar.querySelector(`[data-nav=${sections[index].id}]`);
+      if (activeLink != null) {
+        activeLink.classList.add("highlight-link");
+      }
+    } else {
+      sections[index].classList.remove("your-active-class");
+      activeLink = navBar.querySelector(`[data-nav=${sections[index].id}]`);
+      if (activeLink != null) {
+        activeLink.classList.remove("highlight-link");
+      }
     }
+  }
 }
 
-
-//getting the scrolling to the Top Button 
+//getting the scrolling to the Top Button
 let toTopButton = document.getElementById("topButtonId");
-let topPageCustomElement =  document.getElementById("topOfPage");
+let topPageCustomElement = document.getElementById("topOfPage");
 //calling to the built-in scrollIntoView when clicking event happens
 // {behavior: "smooth"} adds smoothing transition while going to the top of the page.
-  toTopButton.onclick = function () {
-    topPageCustomElement.scrollIntoView({ behavior: "smooth" });
-  };
-
-
+toTopButton.onclick = function () {
+  topPageCustomElement.scrollIntoView({ behavior: "smooth" });
+};
 
 /**
  * End Global letiables
@@ -130,10 +163,6 @@ let topPageCustomElement =  document.getElementById("topOfPage");
 // Build menu
 // Scroll to section on link click
 // Set sections as active
-
-
-
-
 
 /**
 notes for future features to add to the project😀:
